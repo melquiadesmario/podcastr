7a7123ebc2f451dd5e524aa455741381b3fbafc5
melquiadesmario <melquiadesm@gmail.com>
2021-04-21 17:53:56 -0300

    Add Axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { GetStaticProps } from 'next';
 
+import { api } from '../services/api';
+
 type Episode = {
   episodes: Array<{
     id: string;
@@ -23,8 +25,13 @@ export default function Home(props: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json();
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
 
   return{
     props: {
